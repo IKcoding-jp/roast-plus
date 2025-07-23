7392457aa6a847b4ccce6dbbf314f9aae19f3a50
IK <kensaku.ikeda04@gmail.com>
2025-07-24 02:34:01 +0900

    Fix Gradle build issues and update to version 0.5.16

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -27,5 +27,8 @@ tasks.withType<JavaCompile> {
 }
 
 plugins {
+    // org.jetbrains.kotlin.android プラグインは既にクラスパスに存在しているため、ここで再度適用しないようにコメントアウトまたは削除します。
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
+    id("com.android.application") version "8.7.3" apply false
 }
